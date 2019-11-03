1f815cf63846295e8e53e454a484c6cc67cd37da
Renato Yamassaki <renato@yuzu-project.com>
2019-11-03 08:41:08 -0800

    Reformat code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    int port = 8080;
-
-    std::cout << "Hello world, project compilation successful!" << std::endl;
-
-    return 0;
+int main(int argc, const char* argv[]) {
+  int port = 8080;
+  std::cout << "Hello world, project compilation successful!" << std::endl;
+  return 0;
 }
